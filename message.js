662006d6c87472393c6edd8533c632726f1fd2ee
Afolabi Adekunle <49187715+Alchemist7534@users.noreply.github.com>
2020-03-25 16:42:00 +0100

    NodeJs Task one

---
+++ message.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function(req, res){
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.write('<form action="/message" method="post" enctype="application/x-www-form-urlencoded" style="text-align: center;"');
+res.write('<br><textarea cols="35" rows="10" placeholder="your message" name="message"></textarea>');
+res.write('<br><br><button>Send</button>');
+res.write('</form>');
+if (req.method === 'POST') {
+let body = '';
+req.on('data', chunk => {
+body += chunk.toString(); 
+});
+req.on('end', () => {
+fs.appendFileSync('./message.txt', body);
+res.end('ok');
+});
+}
+else {
+return 'error';
+}
+res.end();
+}).listen(8080);
